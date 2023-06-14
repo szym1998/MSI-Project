01521ca2b0623f44a676296de5ba7be0f4002ca9
szym1998 <szymonbogdani@gmail.com>
2023-06-14 15:26:30 +0000

    almost done

--- ploting.py
+++ ploting.py
@@ -1,5 +1,5 @@
 import io
-
+import base64
 import numpy as np
 import pywt
 import os
@@ -13,9 +13,11 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 
-def plot_time_domain(path_to_file, channel=0, line_width=0.5, xlim=None, ylim=None):
+def plot_td(path_to_file, channel=0, line_width=0.5, xlim=None, ylim=None):
     # Get the sample rate and data from the WAV object
     sample_rate, data = x2w(path_to_file)
+    og_xlim = (0, len(data) / sample_rate)
+    og_ylim = [-1, 1]
 
     # If the data array is one-dimensional, set data_channel to data
     if data.ndim == 1:
@@ -23,6 +25,9 @@ def plot_time_domain(path_to_file, channel=0, line_width=0.5, xlim=None, ylim=No
     # Otherwise, extract the specified channel from the data array
     else:
         data_channel = data[:, channel]
+    
+    #normalize data
+    data_channel = data_channel / np.max(np.abs(data_channel))
 
     # Create a time axis for the data
     time_axis = np.arange(len(data_channel)) / float(sample_rate)
@@ -44,8 +49,8 @@ def plot_time_domain(path_to_file, channel=0, line_width=0.5, xlim=None, ylim=No
         ylim = [min_data*1.1, max_data*1.1]
     
     # Store original xlim and ylim
-    orig_xlim = xlim
-    orig_ylim = ylim
+    
+    
 
     #limit xlim and ylim to 2 decimal places
     xlim = [round(x, 2) for x in xlim]
@@ -70,14 +75,42 @@ def plot_time_domain(path_to_file, channel=0, line_width=0.5, xlim=None, ylim=No
     # Reset the buffer's position to the start
     buf.seek(0)
 
-    # Return the BytesIO object along with the original xlim and ylim values
-    return buf, orig_xlim, orig_ylim
+    image_string = base64.b64encode(buf.getvalue()).decode("utf-8")
+
+    # Return the base64 string along with the original xlim, ylim, and clim values
+    result = {
+        "image_string_td": image_string,
+        "xlim_td": og_xlim,
+        "ylim_td": og_ylim,
+        "xlim_td_current": xlim,
+        "ylim_td_current": ylim,
+    }
+
+    return result
 
 
-
-def plot_frequency_domain(path_to_file, channel=0, dpi=100, n_fft=None, hop_length=None, xlim=None, ylim=None, cbar_lim=None):
+def plot_stft(
+    path_to_file,
+    channel=0,
+    dpi=100,
+    n_fft=None,
+    hop_length=None,
+    xlim=None,
+    ylim=None,
+    cbar_lim=None,
+):
     # Get the sample rate and data from the WAV object
     sample_rate, data = x2w(path_to_file)
+
+    if len(data) < 5000:
+        og_n_fft = sample_rate // 20
+    else:
+        og_n_fft = sample_rate // 200
+
+    if len(data) < 5000:
+        og_hop_length = og_n_fft // 1.1
+    else:
+        og_hop_length = og_n_fft // 2
 
     if not n_fft:
         if len(data) < 5000:
@@ -90,6 +123,9 @@ def plot_frequency_domain(path_to_file, channel=0, dpi=100, n_fft=None, hop_leng
         else:
             hop_length = n_fft // 2
 
+    if hop_length > n_fft:
+        hop_length = n_fft // 1.1
+
     # If the data array is one-dimensional, set data_channel to data
     if data.ndim == 1:
         data_channel = data
@@ -98,33 +134,200 @@ def plot_frequency_domain(path_to_file, channel=0, dpi=100, n_fft=None, hop_leng
         data_channel = data[:, channel]
 
     # Compute the STFT of the data
-    f, t, Zxx = signal.stft(data_channel, sample_rate, nperseg=n_fft, noverlap=hop_length)
+    f, t, Zxx = signal.stft(
+        data_channel, sample_rate, nperseg=n_fft, noverlap=hop_length
+    )
 
     # Calculate the original xlim, ylim, and cbar_lim
-    original_xlim = [0, t[-1]]
-    original_ylim = [0, f[-1]]
-    original_cbar_lim = [np.abs(Zxx).min(), np.abs(Zxx).max()]
+    og_xlim = [0, t[-1]]
+    og_ylim = [0, f[-1]]
+    og_clim = [np.abs(Zxx).min(), np.abs(Zxx).max()]
 
     # Create a figure and plot the STFT
     fig, ax = plt.subplots(figsize=(10, 6))  # Adjust the size as needed
     fig.set_dpi(dpi)
 
     # Use the original or provided cbar_lim for the color map
-    cbar_lim = cbar_lim or original_cbar_lim
-    pcm = ax.pcolormesh(t, f, np.abs(Zxx), cmap='viridis', shading='gouraud', rasterized=True, vmin=cbar_lim[0], vmax=cbar_lim[1])
+    cbar_lim = cbar_lim or og_clim
+    pcm = ax.pcolormesh(
+        t,
+        f,
+        np.abs(Zxx),
+        cmap="viridis",
+        shading="gouraud",
+        rasterized=True,
+        vmin=cbar_lim[0],
+        vmax=cbar_lim[1],
+    )
 
     # Set the x and y labels
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Frequency (Hz)')
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Frequency (Hz)")
 
     # Use the original or provided xlim and ylim
-    xlim = xlim or original_xlim
-    ylim = ylim or original_ylim
+    xlim = xlim or og_xlim
+    ylim = ylim or og_ylim
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
 
     # Set the colorbar
     fig.colorbar(pcm, ax=ax)
+    plt.tight_layout()
+
+    # Convert the plot to a BytesIO object
+    buf = io.BytesIO()
+    plt.savefig(buf, format="svg", dpi=dpi)
+    plt.close(fig)
+
+    # Reset the buffer's position to the start
+    buf.seek(0)
+
+    image_string = base64.b64encode(buf.getvalue()).decode("utf-8")
+
+    # Return the base64 string along with the original xlim, ylim, and clim values
+    result = {
+        "image_string_stft": image_string,
+        "xlim_stft": og_xlim,
+        "ylim_stft": og_ylim,
+        "clim_stft": og_clim,
+        "nfft_stft": og_n_fft,
+        "hop_length_stft": og_hop_length,
+    }
+
+    return result
+
+
+def plot_fcwt(path_to_file, channel=0, f0=None, f1=None, fn=None, dpi=100, mor_size=None, xlim=None, ylim=None, clim=None):
+    # Get the sample rate and data from the WAV object
+    sample_rate, data = x2w(path_to_file)
+
+    
+    og_f1 = sample_rate // 2
+
+    if f1 is None:                                            
+        f1 = sample_rate // 2
+        
+    if fn is None:
+        #if len data shorter than 5000 samples, use 1/10 of the data length
+        if len(data) < 5000:
+            fn = len(data)//10
+            original_fn = len(data)//10
+        else:
+            fn = len(data)//500
+            original_fn = len(data)//500
+            
+
+    if mor_size is None:
+        mor_size = sample_rate//1000
+    if f0 is None:
+        f0 = 1
+
+    # If the data array is one-dimensional, set data_channel to data
+    if data.ndim == 1:
+        data_channel = data
+    # Otherwise, extract the specified channel from the data array
+    else:
+        data_channel = data[:, channel]
+
+    # Compute the FCWT of the data
+    morl = fcwt.Morlet(mor_size)
+    scales = fcwt.Scales(morl, fcwt.FCWT_LINFREQS, sample_rate, f0, f1, fn)
+    nthreads = 8
+    use_optimization_plan = False
+    use_normalization = True
+    fcwt_obj = fcwt.FCWT(morl, nthreads, use_optimization_plan, use_normalization)
+    output = np.zeros((fn, data_channel.size), dtype=np.complex64)
+    fcwt_obj.cwt(data_channel, scales, output)
+
+    # Create a figure and plot the FCWT
+    fig, ax = plt.subplots(figsize=(10, 6))  # Adjust the size as needed
+    fig.set_dpi(dpi)
+    time_extent = [0, len(data_channel) / float(sample_rate)]
+    cwt_img = ax.imshow(np.abs(output), extent=[*time_extent, f0, f1], aspect='auto', cmap='viridis')
+
+    # Set the x and y labels
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Frequency (Hz)')
+
+    # Get the original xlim and ylim
+    og_xlim = ax.get_xlim()
+    og_ylim = ax.get_ylim()
+
+    # Set the xlim and ylim if they are provided
+    if xlim:
+        ax.set_xlim(xlim)
+
+    if ylim:
+        ax.set_ylim(ylim)
+
+    # Set the colorbar
+    cbar = fig.colorbar(cwt_img, ax=ax)
+
+    # Get the original clim
+    og_clim = cwt_img.get_clim()
+    plt.tight_layout()
+
+    # Set the color limits if they are provided
+    if clim:
+        cwt_img.set_clim(clim)
+
+    # Convert the plot to a BytesIO object
+    buf = io.BytesIO()
+    plt.savefig(buf, format='svg', dpi=dpi)
+    plt.close(fig)
+
+    # Reset the buffer's position to the start
+    buf.seek(0)
+
+    image_string = base64.b64encode(buf.getvalue()).decode('utf-8')
+
+    # Return the base64 string along with the original xlim, ylim, and clim values
+    result = {
+        "image_string_fcwt": image_string,
+        "xlim_fcwt": og_xlim,
+        "ylim_fcwt": og_ylim,
+        "clim_fcwt": og_clim,
+        "f1_fcwt": og_f1,
+    }
+
+    return result
+
+
+def plot_fft(path_to_file, channel=0, dpi=100, xlim=None, ylim=None):
+    # Get the sample rate and data from the WAV object
+    sample_rate, data = x2w(path_to_file)
+
+    # If the data array is one-dimensional, set data_channel to data
+    if data.ndim == 1:
+        data_channel = data
+    # Otherwise, extract the specified channel from the data array
+    else:
+        data_channel = data[:, channel]
+
+    # Compute the FFT of the data
+    freqs = np.fft.rfftfreq(len(data_channel), 1/sample_rate)
+    fft = np.fft.rfft(data_channel)
+
+    # Create a figure and plot the FFT
+    fig, ax = plt.subplots(figsize=(10, 6), dpi=dpi)
+
+    ax.plot(freqs, np.abs(fft), color='black')
+
+    # Set the x and y labels
+    ax.set_xlabel('Frequency (Hz)')
+    ax.set_ylabel('Amplitude')
+
+    # Get the original xlim and ylim
+    og_xlim = ax.get_xlim()
+    og_ylim = ax.get_ylim()
+
+    # Set the xlim and ylim if they are provided
+    if xlim:
+        ax.set_xlim(xlim)
+
+    if ylim:
+        ax.set_ylim(ylim)
+
     plt.tight_layout()
 
     # Convert the plot to a BytesIO object
@@ -135,8 +338,18 @@ def plot_frequency_domain(path_to_file, channel=0, dpi=100, n_fft=None, hop_leng
     # Reset the buffer's position to the start
     buf.seek(0)
 
-    # Return the BytesIO object along with the original xlim, ylim, and cbar_lim values
-    return buf, original_xlim, original_ylim, original_cbar_lim
+    # Convert the BytesIO object to a base64 string
+    image_string = base64.b64encode(buf.getvalue()).decode('utf-8')
+
+    # Return the base64 string along with the original xlim and ylim values
+    result = {
+        "image_string_fft": image_string,
+        "xlim_fft": og_xlim,
+        "ylim_fft": og_ylim
+    }
+
+    return result
+
 
 
 # def plot_cwt(path_to_file, channel=0, wavelet='morl', scales=np.arange(1, 10000,100), dpi=100, xlim=None, ylim=None, cbar_lim=None):
@@ -193,136 +406,6 @@ def plot_frequency_domain(path_to_file, channel=0, dpi=100, n_fft=None, hop_leng
 
 #     # Return the BytesIO object along with the original xlim, ylim, and cbar_lim values
 #     return buf, original_xlim, original_ylim, original_cbar_lim
-
-
-def plot_fcwt(path_to_file, channel=0, f0=None, f1=None, fn=None, dpi=100, mor_size=None, xlim=None, ylim=None, clim=None):
-    # Get the sample rate and data from the WAV object
-    sample_rate, data = x2w(path_to_file)
-
-    if f1 is None:                                            
-        f1 = sample_rate // 2
-    if fn is None:
-        #if len data shorter than 5000 samples, use 1/10 of the data length
-        if len(data) < 5000:
-            fn = len(data)//10
-        else:
-            fn = len(data)//500
-            
-
-    if mor_size is None:
-        mor_size = sample_rate//1000
-    if f0 is None:
-        f0 = 1
-
-    # If the data array is one-dimensional, set data_channel to data
-    if data.ndim == 1:
-        data_channel = data
-    # Otherwise, extract the specified channel from the data array
-    else:
-        data_channel = data[:, channel]
-
-    # Compute the FCWT of the data
-    morl = fcwt.Morlet(mor_size)
-    scales = fcwt.Scales(morl, fcwt.FCWT_LINFREQS, sample_rate, f0, f1, fn)
-    nthreads = 8
-    use_optimization_plan = False
-    use_normalization = True
-    fcwt_obj = fcwt.FCWT(morl, nthreads, use_optimization_plan, use_normalization)
-    output = np.zeros((fn, data_channel.size), dtype=np.complex64)
-    fcwt_obj.cwt(data_channel, scales, output)
-
-    # Create a figure and plot the FCWT
-    fig, ax = plt.subplots(figsize=(10, 6))  # Adjust the size as needed
-    fig.set_dpi(dpi)
-    time_extent = [0, len(data_channel) / float(sample_rate)]
-    cwt_img = ax.imshow(np.abs(output), extent=[*time_extent, f0, f1], aspect='auto', cmap='viridis')
-
-    # Set the x and y labels
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Frequency (Hz)')
-
-    # Get the original xlim and ylim
-    original_xlim = ax.get_xlim()
-    original_ylim = ax.get_ylim()
-
-    # Set the xlim and ylim if they are provided
-    if xlim:
-        ax.set_xlim(xlim)
-
-    if ylim:
-        ax.set_ylim(ylim)
-
-    # Set the colorbar
-    cbar = fig.colorbar(cwt_img, ax=ax)
-
-    # Get the original clim
-    original_clim = cwt_img.get_clim()
-    plt.tight_layout()
-
-    # Set the color limits if they are provided
-    if clim:
-        cwt_img.set_clim(clim)
-
-    # Convert the plot to a BytesIO object
-    buf = io.BytesIO()
-    plt.savefig(buf, format='svg', dpi=dpi)
-    plt.close(fig)
-
-    # Reset the buffer's position to the start
-    buf.seek(0)
-
-    # Return the BytesIO object along with the original xlim, ylim and clim values
-    return buf, original_xlim, original_ylim, original_clim
-
-
-def plot_fft(path_to_file, channel=0, dpi=100, xlim=None, ylim=None):
-    # Get the sample rate and data from the WAV object
-    sample_rate, data = x2w(path_to_file)
-
-    # If the data array is one-dimensional, set data_channel to data
-    if data.ndim == 1:
-        data_channel = data
-    # Otherwise, extract the specified channel from the data array
-    else:
-        data_channel = data[:, channel]
-
-    # Compute the FFT of the data
-    freqs = np.fft.rfftfreq(len(data_channel), 1/sample_rate)
-    fft = np.fft.rfft(data_channel)
-
-    # Create a figure and plot the FFT
-    fig, ax = plt.subplots(figsize=(10, 6), dpi=dpi)
-
-    ax.plot(freqs, np.abs(fft), color='black')
-
-    # Set the x and y labels
-    ax.set_xlabel('Frequency (Hz)')
-    ax.set_ylabel('Amplitude')
-
-    # Get the original xlim and ylim
-    original_xlim = ax.get_xlim()
-    original_ylim = ax.get_ylim()
-
-    # Set the xlim and ylim if they are provided
-    if xlim:
-        ax.set_xlim(xlim)
-
-    if ylim:
-        ax.set_ylim(ylim)
-
-    plt.tight_layout()
-
-    # Convert the plot to a BytesIO object
-    buf = io.BytesIO()
-    plt.savefig(buf, format='svg', dpi=dpi)
-    plt.close(fig)
-
-    # Reset the buffer's position to the start
-    buf.seek(0)
-
-    # Return the BytesIO object along with the original xlim and ylim values
-    return buf, original_xlim, original_ylim
-
 
 
 # import os
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
+from multiprocessing import Pool
 from flask import Flask, render_template, request, redirect, url_for, flash
 import os
-import ploting as pl
+import ploting as plots
 import base64
 from werkzeug.utils import secure_filename
 
@@ -11,11 +12,11 @@ UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
+
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'wav', 'mp3', 'ogg', 'flac', 'mat', 'csv'}
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -23,17 +24,17 @@ def analysis_up():
     if request.method == 'POST':
         # Check if a file was posted
         if 'file' not in request.files:
-            
+
             return redirect(request.url)
-        
+
         file = request.files['file']
 
         # If user does not select file, the browser might
         # submit an empty part without filename
         if file.filename == '':
-            
+
             return redirect(request.url)
-        
+
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
@@ -47,58 +48,95 @@ def analysis_up():
 def submit_analysis():
     selected_file = request.form.get('file')
     selected_analyses = request.form.getlist('analysis')
-    
+
     if not selected_file or not selected_analyses:
         # Return an error message if no file or analyses were selected
         return render_template('error.html', message="Please select a file and at least one analysis method.")
-    
+
     # Generate the plots here based on the selected file and analyses...
-    
+
     return "Analysis submitted for file: " + selected_file + ", Analyses: " + ", ".join(selected_analyses)
+
 
 @app.route('/upload')
 def upload_file():
     return render_template('upload.html')
+
 
 @app.route('/upload', methods=['POST'])
 def receive_file():
     if 'file' not in request.files:
         return render_template('error.html', message="No file part in the request.")
     file = request.files['file']
-    
+
     if file.filename == '':
         return render_template('error.html', message="No file selected for uploading.")
-    
+
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
     return redirect(url_for('analysis'))
 
 
-
-
-from multiprocessing import Pool
-
 def get_image_string(plot_function, file_path):
     result = plot_function(file_path)
-    image_string = base64.b64encode(result[0].getvalue()).decode('utf-8')  # Get the first returned value (buf) only
-    return image_string
+    return result
 
-
-@app.route('/analysis', methods=['POST'])
+@app.route('/analysis', methods=['GET', 'POST'])
 def analysis():
     file_path = request.form.get('file')
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_path)
-    method = request.form.get('method')
 
-    print(method)
+    time_domain_xlim_str = request.form.get('time_domain_xlim')
+    time_domain_xlim = [float(value) if value else None for value in time_domain_xlim_str.split(',')]
+
+    time_domain_ylim_str = request.form.get('time_domain_ylim')
+    time_domain_ylim = [float(value) if value else None for value in time_domain_ylim_str.split(',')]
+
+    stft_xlim_str = request.form.get('stft_xlim')
+    stft_xlim = [float(value) if value else None for value in stft_xlim_str.split(',')]
+
+    stft_ylim_str = request.form.get('stft_ylim')
+    stft_ylim = [float(value) if value else None for value in stft_ylim_str.split(',')]
+
+    stft_cbar_lim_str = request.form.get('stft_cbar_lim')
+    stft_cbar_lim = [float(value) if value else None for value in stft_cbar_lim_str.split(',')]
+
+    stft_n_fft = float(request.form.get('stft_n_fft')) if request.form.get('stft_n_fft') else None
+    stft_hop_length = float(request.form.get('stft_hop_length')) if request.form.get('stft_hop_length') else None
+
+    fcwt_f0_f1_str = request.form.get('fcwt_f0_f1')
+    fcwt_f0_f1 = [float(value) if value else None for value in fcwt_f0_f1_str.split(',')]
+
+    fcwt_xlim_str = request.form.get('fcwt_xlim')
+    fcwt_xlim = [float(value) if value else None for value in fcwt_xlim_str.split(',')]
+
+    fcwt_ylim_str = request.form.get('fcwt_ylim')
+    fcwt_ylim = [float(value) if value else None for value in fcwt_ylim_str.split(',')]
+
+    fcwt_clim_str = request.form.get('fcwt_clim')
+    fcwt_clim = [float(value) if value else None for value in fcwt_clim_str.split(',')]
+
+    fcwt_fn = float(request.form.get('fcwt_fn')) if request.form.get('fcwt_fn') else None
+    fcwt_mor_size = float(request.form.get('fcwt_mor_size')) if request.form.get('fcwt_mor_size') else None
+
+    fft_xlim_str = request.form.get('fft_xlim')
+    fft_xlim = [float(value) if value else None for value in fft_xlim_str.split(',')]
+
+    fft_ylim_str = request.form.get('fft_ylim')
+    fft_ylim = [float(value) if value else None for value in fft_ylim_str.split(',')]
+
+
+
+    print(fcwt_f0_f1)
 
     # Get the list of WAV files
     wav_files = sorted([f for f in os.listdir(app.config['UPLOAD_FOLDER']) if f.endswith('.wav')])
 
     # Create a multiprocessing Pool
-    pool = Pool(processes=4) # You can set this to the number of cores you want to use.
+    pool = Pool(processes=4)
 
     # Use pool.apply_async to run the plot methods in parallel, getting a multiprocessing.pool.AsyncResult for each one
-    results = [pool.apply_async(get_image_string, args=(plot_function, file_path)) for plot_function in (pl.plot_time_domain, pl.plot_frequency_domain, pl.plot_fcwt, pl.plot_fft)]
+    results = [pool.apply_async(get_image_string, args=(plot_function, file_path)) for plot_function in (
+        plots.plot_td, plots.plot_stft, plots.plot_fcwt, plots.plot_fft)]
 
     # Close the pool to prevent any more tasks from being submitted to the pool.
     pool.close()
@@ -106,11 +144,59 @@ def analysis():
     # Wait for all the tasks to complete
     pool.join()
 
-    # Get the results
-    image_string_time_domain, image_string_stft, image_string_fcwt, image_string_fft = [result.get() for result in results]
+    # Combine all results in a single dictionary
+    results_dict = {}
+    for result in results:
+        results_dict.update(result.get())
 
-    return render_template('analysis.html', files=wav_files, image_string_time_domain=image_string_time_domain, image_string_stft=image_string_stft, image_string_fcwt=image_string_fcwt, image_string_fft=image_string_fft)
+    # Convert tuples to lists within results_dict
+    for key, value in results_dict.items():
+        if isinstance(value, tuple):
+            results_dict[key] = list(value)
 
+    # Extract the required variables from the results_dict
+    image_string_td = results_dict["image_string_td"]
+    image_string_stft = results_dict["image_string_stft"]
+    image_string_fcwt = results_dict["image_string_fcwt"]
+    image_string_fft = results_dict["image_string_fft"]
+
+    xlim_td_var = results_dict["xlim_td"]
+    ylim_td_var = results_dict["ylim_td"]
+    
+    xlim_stft_var = results_dict["xlim_stft"]
+    ylim_stft_var = results_dict["ylim_stft"]
+    clim_stft_var = results_dict["clim_stft"]
+    nfft_stft_var = results_dict["nfft_stft"]
+    hop_length_stft_var = results_dict["hop_length_stft"]
+    xlim_fcwt_var = results_dict["xlim_fcwt"]
+    ylim_fcwt_var = results_dict["ylim_fcwt"]
+    clim_fcwt_var = results_dict["clim_fcwt"]
+    f1_fcwt_var = results_dict["f1_fcwt"]
+    xlim_fft_var = results_dict["xlim_fft"]
+    ylim_fft_var = results_dict["ylim_fft"]
+
+
+    # Pass the results dictionary to the template
+    return render_template('analysis.html', files=wav_files, 
+                        image_string_time_domain=image_string_td, 
+                        image_string_stft=image_string_stft,
+                        image_string_fcwt=image_string_fcwt,
+                        image_string_fft=image_string_fft,
+                        xlim_td=xlim_td_var,
+                        
+                        ylim_td=ylim_td_var,
+                        xlim_stft=xlim_stft_var,
+                        ylim_stft=ylim_stft_var,
+                        clim_stft=clim_stft_var,
+                        nfft_stft=nfft_stft_var,
+                        hop_length_stft=hop_length_stft_var,
+                        xlim_fcwt=xlim_fcwt_var,
+                        ylim_fcwt=ylim_fcwt_var,
+                        clim_fcwt=clim_fcwt_var,
+                        f1_fcwt=f1_fcwt_var,
+                        xlim_fft=xlim_fft_var,
+                        ylim_fft=ylim_fft_var)
+                           
 
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
